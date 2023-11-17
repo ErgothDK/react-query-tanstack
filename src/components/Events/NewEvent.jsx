fb800a3f4ab869df3cf78f62615e59324277ab11
ErgothDK <ergothdk@gmail.com>
2023-11-17 00:40:50 -0600

    Added new event feature

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,40 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { createNewEvent } from "../../util/http.js";
+import { useMutation } from "@tanstack/react-query";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
         <>
           <Link to="../" className="button-text">
             Cancel
           </Link>
-          <button type="submit" className="button">
-            Create
+          <button type="submit" className="button" disabled={isPending}>
+            {isPending ? "Submitting..." : "Create"}
           </button>
         </>
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Failed while creating event"
+          message={error.info ? error.message : "Failed to load the events"}
+        />
+      )}
     </Modal>
   );
 }
